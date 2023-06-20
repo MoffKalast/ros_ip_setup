25402bd115832ef90fe415a37827d3696569485a
MoffKalast <vid.rijavec@gmail.com>
2023-06-20 14:17:30 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-ip="$(hostname -I | awk '{print $2}')"
+ip=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
+i=0
 while [ -z $ip ]
 do
-	sleep 1
-	ip="$(hostname -I | awk '{print $2}')"
+        sleep 1
+        ip=$(ip -4 addr show wlan0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+        ((i++))
+        if [[ $i -eq 30 ]]; then
+                break
+        fi
 done
-
-#export ROS_IP=192.168.1.10
-#export ROS_HOSTNAME=$(hostname).local
-#export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
 
 export ROS_IP="$(echo $ip | xargs)"
 export ROS_HOSTNAME=$ROS_IP
